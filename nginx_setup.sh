d839404d931b35c8a06f3a663aa710c39e2f97a3
Y Axis <75561364+mgtortoise@users.noreply.github.com>
2022-09-16 23:02:16 +0630

    Update nginx_setup.sh

--- nginx_setup.sh
+++ nginx_setup.sh
@@ -1,10 +1,9 @@
 sudo apt install nginx -y
 sudo apt install wget -y
-rm -rf /etc/nginx/sites-enabled/defaultdefault 
-mkdir /etc/nginx/sites-enabled/
+cd /etc/nginx/sites-enabled/default
+rm default 
 wget https://raw.githubusercontent.com/mgtortoise/asdfj/main/default
 mv default /etc/nginx/sites-enabled/default
 cd ..
 nano /etc/nginx/sites-enabled/default
-systemctl restart nginx
-systemctl status nginx
+
