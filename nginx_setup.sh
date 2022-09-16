ed2db60482fb5fe30338c637082a4019a6b3b1c4
Y Axis <75561364+mgtortoise@users.noreply.github.com>
2022-09-16 22:46:28 +0630

    Add files via upload

--- nginx_setup.sh
+++ nginx_setup.sh
@@ -1,3 +1,9 @@
-git clone https://github.com/mgtortoise/asdfj
-cd asdfj
-mv hi.txt /etc/nginx/sites-enabled/default
+sudo apt install nginx -y
+sudo apt install wget -y
+rm -rf /etc/nginx/sites-enabled/default
+wget https://raw.githubusercontent.com/mgtortoise/asdfj/main/default
+mv default /etc/nginx/sites-enabled/default
+cd ..
+nano /etc/nginx/sites-enabled/default
+systemctl restart nginx
+systemctl status nginx
